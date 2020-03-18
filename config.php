324c32c7251184a451f1a9a93e65e459b471c826
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-18 15:32:27 +0800

    Update config.php

--- config.php
+++ config.php
@@ -9,13 +9,13 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID
-$google_client->setClientId('539246690236-tvoad6op2k24t4010o2re4nd9v2kjq1n.apps.googleusercontent.com');
+$google_client->setClientId('693335878324-0ifkl4lsklo1ppe7qopma66qrtpn9afs.apps.googleusercontent.com');
 
 //Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret('WpZooC_GLJZXb9zunI3SH828');
+$google_client->setClientSecret('9NiLEvHOEQkYYWZ18cSOEon8');
 
 //Set the OAuth 2.0 Redirect URI
-$google_client->setRedirectUri('https://maja-etact.herokuapp.com/index.php');
+$google_client->setRedirectUri('https://kitarvin23.herokuapp.com/index.php');
 
 //
 $google_client->addScope('email');
