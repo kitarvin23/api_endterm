9af8d707a8570e7f7f855d050dc5c83c21a98f89
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-18 22:07:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,100 @@ if($login_button == true){
     </head>
     <body>
 
+<nav class="navbar navbar-expand-md navbar-dark bg-info sticky-top">
+
+<button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+  <span class="navbar-toggler-icon" ></span>
+</button>
+
+<div class="collapse navbar-collapse" id="collapse_target">
+<a class="navbar-brand"><img src="images/api.png" width="70" height="auto"></a>
+  <span class="navbar-text-dark">Gallego & Junsay</span>
+
+<ul class="navbar-nav ml-auto">
+<li class="nav-item">
+  
+  <a class="nav-link" href="index.php?navigation=home">Home</a>
+
+</li>
+
+  <li class="nav-item">
+  
+    <a class="nav-link" href="index.php?navigation=product">Product</a>
+
+  </li>
+  
+  <li class="nav-item">
+  
+    <a class="nav-link" href="index.php?navigation=categories">Categories</a>
+
+  </li>
+
+  <li class="nav-item">
+  
+    <a class="nav-link" href="index.php?navigation=create">Create</a>
+
+  </li>
+
+
+</ul>
+
+</div>
+</nav>
+
       <div>         
+      <?php
+       if($login_button == ''){
+        switch($navigation){
+          case 'product':
+            require_once 'product.php';
+            break;
+          case 'categories':
+            require_once 'categories.php';
+            break;
+          case 'create':
+            require_once 'form_create.php';
+            break;
+          case 'details':
+            require_once 'product-details.php';
+            break;
+            case 'update':
+              require_once 'form_update.php';
+              break;
+          default:
+          require_once 'home.php';
+          break;
+        }
+      }else{
+        echo '<div align="center">'.$login_button . '</div>';
+      }
+    ?>
+      <?php
+      if(isset($facebook_login_url)){
+            echo '<div align="center">' .$facebook_login_url. '</div>';
+            }else{
+        switch($navigation){
+          case 'product':
+            require_once 'product.php';
+            break;
+          case 'categories':
+            require_once 'categories.php';
+            break;
+          case 'create':
+            require_once 'form_create.php';
+            break;
+          case 'details':
+            require_once 'product-details.php';
+            break;
+            case 'update':
+              require_once 'form_update.php';
+              break;
+          default:
+          require_once 'home.php';
+          break;
+        }
+      }
+    ?>
     
       </div>
     </body>
