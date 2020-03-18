02fd4211ff64ae962fbc1e13b7b20019608d46c1
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-18 18:01:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php 
 $navigation = (isset($_GET['navigation']) && $_GET['navigation'] != '') ? $_GET['navigation']: '';
-header('location: login.php');
-
+include('google/google-confirm.php');
+if($login_button == true){
+  include('facebook/facebook-confirm.php');
+}
 ?>
 <html> 
     <head>
@@ -53,6 +55,61 @@ header('location: login.php');
 
 </div>
 </nav>
+
+      <div>         
+      <?php
+       if($login_button == ''){
+        switch($navigation){
+          case 'product':
+            require_once 'product.php';
+            break;
+          case 'categories':
+            require_once 'categories.php';
+            break;
+          case 'create':
+            require_once 'form_create.php';
+            break;
+          case 'details':
+            require_once 'product-details.php';
+            break;
+            case 'update':
+              require_once 'form_update.php';
+              break;
+          default:
+          require_once 'home.php';
+          break;
+        }
+      }else{
+        echo '<div align="center">'.$login_button . '</div>';
+      }
+    ?>
+      <?php
+      if(isset($facebook_login_url)){
+            echo '<div align="center">' .$facebook_login_url. '</div>';
+            }else{
+        switch($navigation){
+          case 'product':
+            require_once 'product.php';
+            break;
+          case 'categories':
+            require_once 'categories.php';
+            break;
+          case 'create':
+            require_once 'form_create.php';
+            break;
+          case 'details':
+            require_once 'product-details.php';
+            break;
+            case 'update':
+              require_once 'form_update.php';
+              break;
+          default:
+          require_once 'home.php';
+          break;
+        }
+      }
+    ?>
+    
       </div>
     </body>
 </html>
