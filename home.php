0f9b0ed95bbf1d40fc65095302b1820c2f0161ac
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-18 21:49:01 +0800

    Update home.php

--- home.php
+++ home.php
@@ -24,7 +24,7 @@
 
   <li class="nav-item">
   
-    <a class="nav-link" href="index.php?navigation=product">Product</a>
+    <a class="nav-link" href="home.php?navigation=product">Product</a>
 
   </li>
   
