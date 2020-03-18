33960ee8231065a4d8684cdf9dd8059efd24e20f
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-18 18:23:24 +0800

    Add files via upload

--- product.php
+++ product.php
@@ -1,5 +1,11 @@
+
+<script rel="stylesheet" type="text/javascript" src="css/js/popper.min.js"></script>
+<script rel="stylesheet" type="text/javascript" src="css/js/jquery-3.4.1.slim.min.js"></script>
+<!-- important! dapat ang bootstrap.min.js ara sa dalom sang tanan-->
+<script rel="stylesheet" type="text/javascript" src="css/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="css/style2.css">
 <?php
-//credits: john paul maja :D
 $json = file_get_contents("http://rdapi.herokuapp.com/product/read.php");
 
 $data = json_decode($json,true);
@@ -17,28 +23,42 @@ if(isset($_POST['search'])){
 }
 ?>
 
-<h1> Product List </h1> <!-- credits: john paul maja :D <3 -->
-
-<form action="index.php?navigation=product" method="POST">
-    <input type="text" name="search" placeholder="Search Product Name">
-	<input type="submit" name="submit" value="Search">
+    <form Method="POST" class="example mt-4" action="index.php?navigation=product" style="margin:auto;max-width:300px">
+<input type="text" class="form-control" placeholder="search" name="search">
+    <div class="input-group-append">
+      <button class="btn btn-success ml-auto" type="submit" name="submit">Go</button>  
+     </div>
 </form>
 
-<table><!-- credits: john paul maja :D <3 -->
+
+
+<div class="container">    
+
+<h4> Product List </h4>   
+
+  <table class="table table-striped">
+    <thead>
     <tr>
-        <th>ID</th> 
+        <th>ID</th>
         <th>Product</th>
         <th>Price</th>
     </tr>
-<?php
-foreach($list as $value){
-    ?>
-    <tr>
+    </thead>
+    <tbody>
+   <?php foreach($list as $value)
+{
+?>
+
+<tr>
         <td><?php echo $value['id'];?></td>
         <td><a href="index.php?navigation=details&id=<?php echo $value['id'];?>"><?php echo $value['name'];?></a></td>
         <td><?php echo $value['price'];?></td>
-    </tr> <!-- credits: john paul maja :D <3 -->
+    </tr>
+
 <?php
 }
-    ?>
-</table><!-- credits: john paul maja :D <3 -->
+?>
+     
+    </tbody>
+  </table>
+</div>
