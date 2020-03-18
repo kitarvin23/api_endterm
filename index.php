3719d7d76aca754fbd250f2ed4a63908eeb034db
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-18 21:43:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
-header('location: login.php');
 $navigation = (isset($_GET['navigation']) && $_GET['navigation'] != '') ? $_GET['navigation']: '';
+include('google/google-confirm.php');
+if($login_button == true){
+  include('facebook/facebook-confirm.php');
+}
 ?>
 <html> 
     <head>
@@ -11,47 +14,6 @@ $navigation = (isset($_GET['navigation']) && $_GET['navigation'] != '') ? $_GET[
 <script rel="stylesheet" type="text/javascript" src="css/js/bootstrap.min.js"></script>
     </head>
     <body>
-
-<nav class="navbar navbar-expand-md navbar-dark bg-info sticky-top">
-
-<button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
-  <span class="navbar-toggler-icon" ></span>
-</button>
-
-<div class="collapse navbar-collapse" id="collapse_target">
-<a class="navbar-brand"><img src="images/api.png" width="70" height="auto"></a>
-  <span class="navbar-text-dark">Gallego & Junsay</span>
-
-<ul class="navbar-nav ml-auto">
-<li class="nav-item">
-  
-  <a class="nav-link" href="index.php?navigation=home">Home</a>
-
-</li>
-
-  <li class="nav-item">
-  
-    <a class="nav-link" href="index.php?navigation=product">Product</a>
-
-  </li>
-  
-  <li class="nav-item">
-  
-    <a class="nav-link" href="index.php?navigation=categories">Categories</a>
-
-  </li>
-
-  <li class="nav-item">
-  
-    <a class="nav-link" href="index.php?navigation=create">Create</a>
-
-  </li>
-
-
-</ul>
-
-</div>
-</nav>
 
       <div>         
       <?php
