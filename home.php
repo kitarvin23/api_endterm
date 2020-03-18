0abbf036d9977ef3bd9896269f776398f70f36b8
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-18 21:52:15 +0800

    Update home.php

--- home.php
+++ home.php
@@ -5,47 +5,6 @@
 <script rel="stylesheet" type="text/javascript" src="css/js/bootstrap.min.js"></script>
 
 
-<nav class="navbar navbar-expand-md navbar-dark bg-info sticky-top">
-
-<button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
-  <span class="navbar-toggler-icon" ></span>
-</button>
-
-<div class="collapse navbar-collapse" id="collapse_target">
-<a class="navbar-brand"><img src="images/api.png" width="70" height="auto"></a>
-  <span class="navbar-text-dark">Gallego & Junsay</span>
-
-<ul class="navbar-nav ml-auto">
-<li class="nav-item">
-  
-  <a class="nav-link" href="index.php?navigation=home">Home</a>
-
-</li>
-
-  <li class="nav-item">
-  
-    <a class="nav-link" href="home.php?navigation=product">Product</a>
-
-  </li>
-  
-  <li class="nav-item">
-  
-    <a class="nav-link" href="index.php?navigation=categories">Categories</a>
-
-  </li>
-
-  <li class="nav-item">
-  
-    <a class="nav-link" href="index.php?navigation=create">Create</a>
-
-  </li>
-
-
-</ul>
-
-</div>
-</nav>
-
 <!------ Include the above in your HEAD tag ---------->
 <div class="container" style="margin-left: 25%;">
 	<div class="row">
