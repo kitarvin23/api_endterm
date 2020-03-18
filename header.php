77ec538690af191442ee67de70438399f19c7a8a
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-18 21:54:51 +0800

    Update header.php

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <ul class="navbar-nav ml-auto">
 <li class="nav-item">
   
-  <a class="nav-link" href="index.php?navigation=home">Home</a>
+  <a class="nav-link" href="header.php?navigation=home">Home</a>
 
 </li>
 
