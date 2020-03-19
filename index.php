c8d3005fd0bcd3253c4190d4762c0c4b5f9bb851
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-19 22:56:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -107,7 +107,7 @@ if($login_button == true){
           break;
          }
         }else{
-        echo '<div align="center">'.$login_button . '</div>';
+        echo ' ';
       }
     ?>
       
