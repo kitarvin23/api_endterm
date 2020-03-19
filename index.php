99aef678a493b80fbfc9070e4b14a745ab2fd77f
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-19 22:32:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -57,32 +57,7 @@ if($login_button == true){
 </nav>
 
       <div>       
-        <?php
-       if($login_button == ''){
-        switch($navigation){
-          case 'product':
-            require_once 'product.php';
-            break;
-          case 'categories':
-            require_once 'categories.php';
-            break;
-          case 'create':
-            require_once 'form_create.php';
-            break;
-          case 'details':
-            require_once 'product-details.php';
-            break;
-            case 'update':
-              require_once 'form_update.php';
-              break;
-          default:
-          require_once 'home.php';
-          break;
-         }
-        }else{
-        echo '<div align="center">'.$login_button . '</div>';
-      }
-    ?>
+        
         <?php
        if($facebook_login_url == ''){
         switch($navigation){
@@ -107,6 +82,32 @@ if($login_button == true){
          }
         }else{
         echo '<div align="center">'.$facebook_login_url . '</div>';
+      }
+    ?>
+        <?php
+       if($login_button == ''){
+        switch($navigation){
+          case 'product':
+            require_once 'product.php';
+            break;
+          case 'categories':
+            require_once 'categories.php';
+            break;
+          case 'create':
+            require_once 'form_create.php';
+            break;
+          case 'details':
+            require_once 'product-details.php';
+            break;
+            case 'update':
+              require_once 'form_update.php';
+              break;
+          default:
+          require_once 'home.php';
+          break;
+         }
+        }else{
+        echo '<div align="center">'.$login_button . '</div>';
       }
     ?>
       
