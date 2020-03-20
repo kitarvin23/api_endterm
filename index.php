45c5413772bd33a9c950963d14724d608b21a23d
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-20 13:41:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,45 +13,6 @@ if($login_button == true){
 <script rel="stylesheet" type="text/javascript" src="css/js/jquery-3.4.1.slim.min.js"></script>
 <script rel="stylesheet" type="text/javascript" src="css/js/bootstrap.min.js"></script>
     </head>
-    <body>
-
-<nav class="navbar navbar-expand-md navbar-dark bg-info sticky-top">
-
-<button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
-  <span class="navbar-toggler-icon" ></span>
-</button>
-
-<div class="collapse navbar-collapse" id="collapse_target">
-<a class="navbar-brand"><img src="images/api.png" width="70" height="auto"></a>
-  <span class="navbar-text-dark">Gallego & Junsay</span>
-
-<ul class="navbar-nav ml-auto">
-<li class="nav-item">
-  
-  <a class="nav-link" href="index.php?navigation=home">Home</a>
-
-</li>
-
-  <li class="nav-item">
-  
-    <a class="nav-link" href="index.php?navigation=product">Product</a>
-
-  </li>
-  
-  <li class="nav-item">
-  
-    <a class="nav-link" href="index.php?navigation=categories">Categories</a>
-
-  </li>
-
-  <li class="nav-item">
-  
-    <a class="nav-link" href="index.php?navigation=create">Create</a>
-
-  </li>
-
-
-</ul>
 
 </div>
 </nav>
