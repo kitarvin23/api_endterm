89edb2ae3a608c9e4852f1bfcaa6bf01ba9533e3
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-20 11:16:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -57,33 +57,6 @@ if($login_button == true){
 </nav>
       <div>       
         
-        <?php
-       if($facebook_login_url == '' OR $login_button == ''){
-        switch($navigation){
-          case 'product':
-            require_once 'product.php';
-            break;
-          case 'categories':
-            require_once 'categories.php';
-            break;
-          case 'create':
-            require_once 'form_create.php';
-            break;
-          case 'details':
-            require_once 'product-details.php';
-            break;
-            case 'update':
-              require_once 'form_update.php';
-              break;
-          default:
-          require_once 'home.php';
-          break;
-         }
-        }else{
-        echo '<div align="center">'.$facebook_login_url . '</div>';
-        echo '<div align="center">'.$login_button . '</div>';
-      }
-    ?>
       </div>
     </body>
 </html>
