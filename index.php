2648d94bd047640db1a8393ad3344650c7691e5d
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-20 15:29:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,100 +1,103 @@
 <?php 
 $navigation = (isset($_GET['navigation']) && $_GET['navigation'] != '') ? $_GET['navigation']: '';
-//credits: john paul maja :D <3
-
-  include('google/google-confirm.php');
-  if($login_button == true){
-    include('facebook/facebook-confirm.php');
-  }
+include('google/google-confirm.php');
+if($login_button == true){
+  include('facebook/facebook-confirm.php');
+}
 ?>
-
-
 <html> 
-    <head> <!-- credits: john paul maja :D <3 -->
-      <title>Endterm Exam</title>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-      <link rel="stylesheet" type="text/css" href="css/style.css">
-      <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan|Fira+Sans|Mukta&display=swap" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan+2&display=swap" rel="stylesheet">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-      
-    </head> 
-    <body> <!-- credits: john paul maja :D <3 -->
-      <div class="header">
-      <a href="index.php"><img src="images/logo.png" style="width:300px;height:200px;"></a>
-      </div>
+    <head>
+      <title>API</title>
+      <link rel="stylesheet" type="text/css" href="css/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/navbar.css">
+<script rel="stylesheet" type="text/javascript" src="css/js/jquery-3.4.1.slim.min.js"></script>
+<script rel="stylesheet" type="text/javascript" src="css/js/bootstrap.min.js"></script>
+    </head>
+  <body>
+      <div>       
+          <?php
+       if($facebook_login_url == '' OR $login_button == ''){ ?>
+        
+<nav class="navbar navbar-expand-md navbar-dark bg-info sticky-top">
 
-      <div class="navbar">
-        <a href="index.php">Home</a>
-        <a href="index.php?navigation=product">Products</a>
-        <a href="index.php?navigation=categories">Category</a>
-        <a href="index.php?navigation=create">Create</a>
-      </div>
+<button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+  <span class="navbar-toggler-icon" ></span>
+</button>
+
+<div class="collapse navbar-collapse" id="collapse_target">
+<a class="navbar-brand"><img src="images/api.png" width="70" height="auto"></a>
+  <span class="navbar-text-dark">Gallego & Junsay</span>
+
+<ul class="navbar-nav ml-auto">
+<li class="nav-item">
+  
+  <a class="nav-link" href="index.php?navigation=home">Home</a>
+
+</li>
+
+  <li class="nav-item">
+  
+    <a class="nav-link" href="index.php?navigation=product">Product</a>
+
+  </li>
+  
+  <li class="nav-item">
+  
+    <a class="nav-link" href="index.php?navigation=categories">Categories</a>
+
+  </li>
+
+  <li class="nav-item">
+  
+    <a class="nav-link" href="index.php?navigation=create">Create</a>
+
+  </li>
 
 
-        <?php //google ------ credits: john paul maja :D <3
-        if($login_button == ''){
-          switch($navigation){
-            case 'product':
-              require_once 'product.php';
-              break;
-            case 'categories':
-              require_once 'categories.php';
-              break;
-            case 'create':
-              require_once 'form_create.php';
-              break;
-            case 'details':
-              require_once 'product-details.php';
-              break;
+</ul>
+</div>
+</nav>
+       <?php switch($navigation){
+          case 'product':
+            require_once 'product.php';
+            break;
+          case 'categories':
+            require_once 'categories.php';
+            break;
+          case 'create':
+            require_once 'form_create.php';
+            break;
+          case 'details':
+            require_once 'product-details.php';
+            break;
             case 'update':
               require_once 'form_update.php';
               break;
-            default:
-              require_once 'home.php';
-              break;
-          }
-        }else{
-          echo '<div align="center">'.$login_button . '</div>';
-        }
+          default:
+          require_once 'home.php';
+          break;
+         } ?>
+  <?php }else{ ?>
+        <nav class="navbar navbar-expand-md navbar-dark bg-info sticky-top">
 
-        ?>
-        <br>
-        <?php //facebook
-            if(isset($facebook_login_url)){
-            echo '<div align="center">' .$facebook_login_url. '</div>';
-            }else{
-              switch($navigation){
-                case 'product':
-                  require_once 'product.php';
-                  break;
-                case 'categories':
-                  require_once 'categories.php';
-                  break;
-                case 'create':
-                  require_once 'form_create.php';
-                  break;
-                case 'details':
-                  require_once 'product-details.php';
-                  break;
-                case 'update':
-                  require_once 'form_update.php';
-                  break;
-                default:
-                  require_once 'home.php';
-                  break;
-              }
-            }
-          
-        ?>
+<button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+  <span class="navbar-toggler-icon" ></span>
+</button>
 
-      <div class="footer">
-        <h1>Maja | API</h2> <!-- credits: john paul maja :D <3 -->
+<div class="collapse navbar-collapse" id="collapse_target">
+<a class="navbar-brand"><img src="images/api.png" width="70" height="auto"></a>
+  <span class="navbar-text-dark">Please log in </span>
+          </div>
+        </nav>
+        </br>
+     <?php   echo '<div align="center">'.$facebook_login_url . '</div>';
+        echo '<div align="center">'.$login_button . '</div>';
+      } ?>
+    
+
+
       </div>
-    </body><!-- credits: john paul maja :D <3 -->
+    </body>
 </html>
 
-
-
-
+  
