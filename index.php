b8a4d2ba15ca4cb393872d8c01110e0ac5ac748a
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-03-20 13:31:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
 $navigation = (isset($_GET['navigation']) && $_GET['navigation'] != '') ? $_GET['navigation']: '';
+include('google/google-confirm.php');
+if($login_button == true){
+  include('facebook/facebook-confirm.php');
 ?>
 <html> 
     <head>
@@ -56,3 +59,33 @@ $navigation = (isset($_GET['navigation']) && $_GET['navigation'] != '') ? $_GET[
       </div>
     </body>
 </html>
+
+    <?php
+       if($facebook_login_url == '' OR $login_button == ''){
+       header('location: index.php?navigation=home');
+        switch($navigation){
+          case 'product':
+            require_once 'product.php';
+            break;
+          case 'categories':
+            require_once 'categories.php';
+            break;
+          case 'create':
+            require_once 'form_create.php';
+            break;
+          case 'details':
+            require_once 'product-details.php';
+            break;
+            case 'update':
+              require_once 'form_update.php';
+              break;
+          default:
+          require_once 'home.php';
+          break;
+         }
+        }else{
+        echo '<div align="center">'.$facebook_login_url . '</div>';
+        echo '<div align="center">'.$login_button . '</div>';
+      }
+    ?>
+
